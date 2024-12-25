12e725ec21131a1ed062a6832ef47f9b164c71c2
eutagocode <thiagoluix21@gmail.com>
2024-12-25 02:37:18 -0300

    feat: :sparkles: Foi incrementada a rota para remover uma venda da meta

--- routes/goalRouter.js
+++ routes/goalRouter.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { goalControllers, updateSold } from "../controllers/goalControllers.js";
+import {
+    goalControllers,
+    updateSold,
+    removeSold,
+} from "../controllers/goalControllers.js";
 
 const router = express.Router();
 
 router.post("/creategoal", goalControllers);
 
 router.post("/updatesold/:id", updateSold);
+
+router.delete("/removesold/:id", removeSold);
 
 export default router;
