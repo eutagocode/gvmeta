992a95825ca4e0c85190567fdc2c40c3b18bc760
eutagocode <thiagoluix21@gmail.com>
2024-12-26 21:44:45 -0300

    feat: :sparkles: Foi feita a importação do cors no arquivo app e o mesmo foi passado como middleware para todas as rotas

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import cors from "cors";
 
 import userRouter from "./routes/userRouter.js";
 import goalRouter from "./routes/goalRouter.js";
@@ -9,6 +10,7 @@ dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use("/", express.json());
 app.use("/gvmeta", userRouter);
 app.use("/gvmeta", goalRouter);
